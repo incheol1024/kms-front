da86302285f5566c9070117dae2da11c95b77af2
yukyongsoo <yongsoo@terafunding.com>
2019-07-20 17:36:08 +0900

    add view,component

--- public/login.js
+++ public/login.js
@@ -1,6 +1,7 @@
 "use strict";
 
 Vue.use(Vuetify);
+const serverUrl = "localhost:8089/login";
 
 new Vue({
 	data: function data() {
@@ -16,7 +17,7 @@ new Vue({
 			let bodyFormData = new FormData();
 			bodyFormData.set('username', this.name);
 			bodyFormData.set('password', this.password);
-			axios.post('/login', bodyFormData).then(function (response) {
+			axios.post(serverUrl, bodyFormData).then(function (response) {
 				window.location.replace("/");
 			}).catch(function (error) {
 				alert("로그인 실패!!! 관리자에게 문의하세요." + JSON.stringify(error.response.data));
---
+++ src/views/dashboard/project-component.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-layout column>
+        <chart-component></chart-component>
+    </v-layout>
+</template>
+
+<script>
+    module.exports = {}
+</script>
+
+<style scoped>
+
+</style>
