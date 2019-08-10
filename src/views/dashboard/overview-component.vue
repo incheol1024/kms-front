15ed6a0318e9e1b475c9e610bfb45a2ec4dae86c
yukyongsoo <yongsoo@terafunding.com>
2019-08-10 16:03:11 +0900

    version mig

--- src/views/dashboard/overview-component.vue
+++ src/views/dashboard/overview-component.vue
@@ -2,20 +2,18 @@
   <v-card>
     <v-list two-line class="mb-5">
       <template v-for="(item, index) in items">
-        <v-subheader v-if="item.header" :key="item.header">{{ item.header }}</v-subheader>
 
+        <v-subheader v-if="item.header" :key="item.header">{{ item.header }}</v-subheader>
+        
         <v-divider v-else-if="item.divider" :key="index" :inset="item.inset"></v-divider>
 
-        <v-list-tile v-else :key="item.title" avatar>
-          <v-list-tile-avatar>
-            <img :src="item.avatar">
-          </v-list-tile-avatar>
+        <v-list-item v-else :key="item.title">
+          <v-list-item-content>
+            <v-list-item-title>{{item.title}}</v-list-item-title>
+            <v-list-item-subtitle>{{ item.subtitle }}</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
 
-          <v-list-tile-content>
-            <v-list-tile-title>{{item.title}}</v-list-tile-title>
-            <v-list-tile-sub-title>{{ item.subtitle }}</v-list-tile-sub-title>
-          </v-list-tile-content>
-        </v-list-tile>
       </template>
     </v-list>
   </v-card>
@@ -27,19 +25,16 @@ export default {
     items: [
       { header: "Today Announce" },
       {
-        avatar: "https://cdn.vuetifyjs.com/images/lists/1.jpg",
         title: "EDM Bug Patch 4.1.1",
         subtitle: "와 AIX 안됨.."
       },
       { divider: true, inset: true },
       {
-        avatar: "https://cdn.vuetifyjs.com/images/lists/2.jpg",
         title: "ECM New Feature 6.1.2",
         subtitle: "마이그 자동화"
       },
       { divider: true, inset: true },
       {
-        avatar: "https://cdn.vuetifyjs.com/images/lists/3.jpg",
         title: "게맛",
         subtitle: "간장게장~~"
       }
