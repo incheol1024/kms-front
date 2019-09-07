93b0820165e2269cd5b7a90d1770269e19cc8f55
yongsoo <yozx9079@naver.com>
2019-09-07 20:14:47 +0900

    change component for search

--- src/views/search-component.vue
+++ src/views/search-component.vue
@@ -1,38 +1,37 @@
 <template>
   <v-layout column>
-    <v-flex v-for="data in searchData" :key="data.id">
-      <v-card>
-        <v-card-title primary-title>
-          <div>
-            <div class="headline">{{data.name}}</div>
-            <span></span>
-          </div>
-        </v-card-title>      
-      </v-card>
-    </v-flex>
+    <v-card v-for="data in searchData" :key="data.id" class="mx-auto" outlined>
+      <v-list-item three-line>
+        <v-list-item-content>
+          <div class="overline mb-4">{{data.time}}</div>
+          <v-list-item-title class="headline mb-1">{{data.name}}</v-list-item-title>
+          <v-list-item-subtitle>{{data.user}}</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-card>
   </v-layout>
 </template>
 
 <script>
-import api from "@/apis/api"
+import api from "@/apis/api";
 
 export default {
-  props : ['word'],
+  props: ["word"],
   data: () => ({
     searchData: []
   }),
-  watch : {
-    word(){
-      this.search()
+  watch: {
+    word() {
+      this.search();
     }
   },
-  created(){
-      this.search()
+  created() {
+    this.search();
   },
-  methods :{
-    async search(){
-      let response = await api.searchFts(this.word)
-      this.searchData = response.data.content
+  methods: {
+    async search() {
+      let response = await api.searchFts(this.word);
+      this.searchData = response.data.content;
     }
   }
 };
